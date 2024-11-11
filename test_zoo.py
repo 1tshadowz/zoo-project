2abc3fe6d2b55a056a57218a9e2be24ae3d23b8c
s.np_s <86773207+1tshadowz@users.noreply.github.com>
2024-11-11 05:03:07 +0000

    Lab Test Exercise Finished

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,17 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    def test_child_ticket_price1(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    def test_child_ticket_price2(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 0)
+    def test_child_ticket_price3(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    def test_child_ticket_price4(self):    
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+    def test_child_ticket_price4(self):
+        self.assertEqual(self.zoo.get_ticket_price(161), 100)
+
 
 if __name__ == '__main__':
     unittest.main()
